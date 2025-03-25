d192dd19b1e1f327ccf22dd42f0d902f8dffb9b8
Akarsh <kakarsh3@gmail.com>
2025-03-26 04:14:41 +0530

    home tab finished

--- lib/src/features/authentication/screens/dashboard_screen/home_tab/home_tab.dart
+++ lib/src/features/authentication/screens/dashboard_screen/home_tab/home_tab.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login/src/features/authentication/screens/profile_screen/profile_screen.dart';
@@ -8,47 +10,21 @@ class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    List color = [
-      Colors.yellow,
-      Colors.green,
-      Colors.red,
-      Colors.blue,
-      Colors.grey,
-      Colors.orange,
-      Colors.cyanAccent,
-      Colors.greenAccent,
-      Colors.deepPurpleAccent,
-      Colors.brown,
-      Colors.pink,
-      Colors.teal
-    ];
-    List color2 = [
-      Colors.teal,
-      Colors.pink,
-      Colors.brown,
-      Colors.deepPurpleAccent,
-      Colors.greenAccent,
-      Colors.cyanAccent,
-      Colors.orange,
-      Colors.grey,
-      Colors.blue,
-      Colors.red,
-      Colors.green,
-      Colors.yellow
-    ];
     return SafeArea(
         child: Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.fromLTRB(4,8,4,0),
         child: SizedBox(
-          child: Stack(alignment: Alignment.center,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
               Positioned(
                   top: 0,
                   right: 0,
                   child: GestureDetector(
                       onTap: () {
-                        Get.to(()=> const ProfileScreen(),transition:Transition.rightToLeftWithFade );
+                        Get.to(() => const ProfileScreen(),
+                            transition: Transition.rightToLeftWithFade);
                       },
                       child: Icon(
                         Icons.account_circle,
@@ -63,46 +39,38 @@ class HomeTab extends StatelessWidget {
                         Icons.menu,
                         size: 40,
                       ))),
-              Positioned(top: 0, child: Text("Latest",style: Theme.of(context).textTheme.headlineSmall,)),
+              Positioned(
+                  top: 0,
+                  child: Text(
+                    "Latest",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  )),
               Container(
-                margin: EdgeInsets.only(top: 50),
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemBuilder: (context, index) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Column(
-                            children: [
-                              Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: color[index],),
-
-                                height: 400,
-                                width: size.width * 0.45,
-                              )
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(color: color2[index],borderRadius: BorderRadius.circular(15)),
-                                
-                                height: 400,
-                                width: size.width * 0.45,
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                  itemCount: color.length,
-                ),
-              )
+                  margin: EdgeInsets.only(top: 50),
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection("wallpaper")
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: (size.width/2.5)/(size.height/2.5),crossAxisSpacing: 8,mainAxisSpacing: 8,),
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, i) {
+                            var lower = snapshot.data!.docs[i]['lower'];
+                            return ClipRRect(borderRadius: BorderRadius.circular(18), child: CachedNetworkImage(imageUrl: lower , fit: BoxFit.fill,));
+                          },
+                        );
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else {
+                        return Center(
+                          child: Text("Error"),
+                        );
+                      }
+                    },
+                  ))
             ],
           ),
         ),
